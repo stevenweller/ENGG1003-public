5646ab97e0d72fc6e7557c2ee0d7ac404da4e389
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-19 12:42:12 +1100

    Create LSlinefitData.py

---
+++ lecture code/LSlinefitData.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([20.5, 32.7, 51.0, 73.2, 95.7])    # temp (degC)
+y = np.array([765, 826, 873, 942, 1032])  # resistance (ohms)
+
+plt.plot(x, y, '.', markersize=10)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
