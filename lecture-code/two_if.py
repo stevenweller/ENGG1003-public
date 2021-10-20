654edf9913546a409fd9e77b62f3f108e688198a
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-20 17:19:40 +1100

    Create two_if.py

---
+++ lecture-code/two_if.py
@@ -0,0 +1,6 @@
+T = float(input('What is the water temperature? '))
+if T > 24:
+    print('Great, jump in!')
+if T <= 24:
+    print('Do not swim. Too cold!')
+# first line after if-if construction
