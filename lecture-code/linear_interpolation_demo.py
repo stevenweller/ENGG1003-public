28d95108a40c1e2472b556409d3d7255aaa8bdcb
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-21 16:34:51 +1100

    Update linear_interpolation_demo.py

--- lecture-code/linear_interpolation_demo.py
+++ lecture-code/linear_interpolation_demo.py
@@ -6,13 +6,13 @@ from scipy.interpolate import interp1d
 np.random.seed(27101967)
 
 N = 11
-t = np.linspace(0,100,N)            # 0,10,20,...,100
-tnew = np.linspace(0,100,100*N)     # 0,0.1,0.2,...,100
-n = np.random.uniform(-25,25,N)     # noise on linear function
+t = np.linspace(0,100,N)        # 0,10,20,...,100
+tnew = np.linspace(0,100,100*N) # 0,0.1,0.2,...,100
+n = np.random.uniform(-25,25,N) # noise on linear function
 
-m = 3                               # gradient
-b = 50                              # intercept
-y = m*t + b + n                     # dataset is straight line + noise
+m = 3                           # gradient
+b = 50                          # intercept
+y = m*t + b + n                 # dataset is straight line + noise
 
 # INTERPOLATION
 f = interp1d(t, y)
