73ce917e01eec0381e46552e16bbfe8fc148fffd
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-21 17:23:53 +1100

    created image_colour.py

---
+++ lecture-code/image_colour.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# create a 3D numpy array, 20x20 RGB image
+m = np.zeros([20, 20, 3], int)
+
+# purple cross on a red background
+m[:, :] = [255, 0, 0]       # set all pixels red by default
+m[8:12, :] = [65, 0, 125]   # rows 8-11 are purple
+m[:, 8:12] = [65, 0, 125]   # columns 8-11 are purple
+
+plt.imshow(m)
+plt.axis('off')
+plt.savefig('purple_cross.jpeg')
+plt.show()
