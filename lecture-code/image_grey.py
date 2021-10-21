22c78fe156cea9495ddc6bc7682ca08cc6fd59db
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-21 17:01:48 +1100

    Update image_grey.py

--- lecture-code/image_grey.py
+++ lecture-code/image_grey.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 m = np.zeros([10, 10])
-m[4:6, :] = 1       # set columns 4:6
-m[:, 4:6] = 1       # set columns 4:6
+m[4:6, :] = 1       # set rows 4 and 5
+m[:, 4:6] = 1       # set columns 4 and 5
 
 # show the image in a figure
 plt.imshow(m, cmap='Greys')
