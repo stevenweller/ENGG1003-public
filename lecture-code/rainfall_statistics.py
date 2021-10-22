35e1db7932fa5804bda974e6c42c9d5576fc29c6
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-22 12:44:47 +1100

    Create rainfall_statistics.py

---
+++ lecture-code/rainfall_statistics.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# read rainfall data from spreadsheet
+rainfallData = pd.read_csv("Rainfall.csv")
+print(rainfallData.head())
+
+# extract columns for years 2010 and 2020
+rainfall2010 = rainfallData['Rainfall 2010'].values
+rainfall2020 = rainfallData['Rainfall 2020'].values
+
+N = len(rainfall2010)       # total number of regions
+
+# count regions where rainfall has decreased between 2010 and 2020
+decCount = 0
+for i in range(0, N):
+    if rainfall2020[i] < rainfall2010[i]:
+        decCount += 1
+
+decPercent = 100 * decCount/N
+print('Out of {} regions, {} have reduced rainfall which is {:5.2f}%'
+      .format(N, decCount, decPercent))
