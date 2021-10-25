04b98edf7e18193a29f4a861cd7cb6fbb941e315
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-25 17:09:13 +1100

    Update pandas_demo.py

--- lecture-code/pandas_demo.py
+++ lecture-code/pandas_demo.py
@@ -7,7 +7,3 @@ print(mydata.head())
 # extract columns with username and student ID
 username = mydata['Username'].values
 ID = mydata['Student Identifier'].values
-
-# display username and ID data
-print(username)
-print(ID)
