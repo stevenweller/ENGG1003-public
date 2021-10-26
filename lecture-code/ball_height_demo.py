afdd39e5d4501a74c122899ffdd26c6cef56aead
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-26 16:45:57 +1100

    Create ball_height_demo.py

---
+++ lecture-code/ball_height_demo.py
@@ -0,0 +1,15 @@
+# function definition
+def ball_height(v0, t):     # function header
+    g = 9.81
+    y = v0*t - 0.5*g*t**2
+    return y
+
+
+v0 = 5
+time1 = 0.6
+height1 = ball_height(v0, time1)
+print(height1)
+
+time2 = 0.9
+height2 = ball_height(v0, time2)
+print(height2)
