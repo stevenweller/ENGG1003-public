7725c066641e6f5162550173433df53ceb6e6e46
stevenweller <78775247+stevenweller@users.noreply.github.com>
2021-10-26 13:21:55 +1100

    Update random_demo.py

--- lecture-code/random_demo.py
+++ lecture-code/random_demo.py
@@ -1,6 +1,6 @@
 import random
 
 # random() function in random library returns a
-# random float number between 0 and 1
+# random float number between between 0.0 and 1.0
 x = random.random()
 print(x)
