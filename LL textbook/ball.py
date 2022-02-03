89d5c5f3a772d5d2ca3fff444f070903c85284e3
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-02-03 11:55:18 +1100

    ball code with better comments

--- LL textbook/ball.py
+++ LL textbook/ball.py
@@ -1,10 +1,10 @@
 # compute height of a ball in vertical motion
 # SRW, 3-Feb-2022
 
-v0 = 5             # initial velocity (m/s)
-g = 9.81           # acceleration due to gravity (m/s^2)
-t = 0.6            # time (s)
+v0 = 5      # initial velocity (m/s)
+g = 9.81    # acceleration due to gravity (m/s^2)
+t = 0.6     # time (s)
 
-y = v0*t - 0.5*g*t**2        # vertical position (m)
+y = v0*t - 0.5*g*t**2   # vertical position (m)
 
 print(y)
