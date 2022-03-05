9067fc7e324b13773476fb42ff7dc4d274176831
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-03-05 15:48:03 +1100

    rewrite for 2022

--- lecture-code/nested_for.py
+++ lecture-code/nested_for.py
@@ -1,6 +1,4 @@
-for i in [1, 2, 3]:
-    # first indentation level (outer loop)
-    print('i = {:d}'.format(i))
-    for j in [4.0, 5.0, 6.0]:
-        # second indentation level (inner loop)
-        print('      j = {:.1f}'.format(j))
+for i in range(1, 4):
+    print("i = {}".format(i))
+    for j in range(4, 7):
+        print("    j = {}".format(j))
