9335ca2d5f13b5e43bb5b94bac74ae6969c40892
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-03-17 21:49:57 +1100

    first

---
+++ lecture-code/coin_seed.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# each run generates identical coin tosses
+np.random.seed(10)
+
+# 10 random integers from {0,1}
+# 0==heads, 1==tails
+x = np.random.randint(0, 2, size=10)
+print(x)
