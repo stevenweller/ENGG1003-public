0621486ad4f9455f0ea04b5c5b92cb8e61626e7e
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-03-31 22:12:45 +1100

    first

---
+++ lecture-code/create_array2D.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+m1 = np.zeros([2, 3])
+m2 = np.array([[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]])
+# m2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+print(m1)
+print(m2)
