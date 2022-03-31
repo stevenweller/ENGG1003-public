700b7b58a47e80cf6973e28d73ef97e7986c6549
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-03-31 22:06:50 +1100

    first

---
+++ lecture-code/create_array1D.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a1 = np.zeros(4)
+a2 = np.linspace(0, 3, num=4)
+a3 = np.array([3, -1.9, 4.8, 6.2])
+
+print(a1)       # [0. 0. 0. 0.]
+print(a2)       # [0. 1. 2. 3.]
+print(a3)       # [ 3.  -1.9  4.8  6.2]
