6ac10b07c2cec247500e49af0687730e101a4ba7
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-04-28 22:03:39 +1000

    first

--- lecture-code/bisection.py
+++ lecture-code/bisection.py
@@ -1,20 +1,25 @@
-def f(L):
-    return 0.0268*L**3 + 1.884*L**2 + 44.15*L - 500
+import numpy as np
 
-eps = 1e-8
-x_LO = 6
-x_HI = 10
+def f(x):
+    return np.exp(x) - 3
 
-x_MID = (x_LO + x_HI)/2
-itCnt = 0
-while abs(f(x_MID)) > eps:
-    if f(x_MID)*f(x_LO) > 0:
-        x_LO = x_MID
+# def f(x):
+#     return 3*x + np.sin(x) - np.exp(x)
+
+eps = 1e-6
+xLO = 1
+xHI = 2
+
+xMID = (xLO + xHI) / 2
+iters = 0
+while abs(f(xMID)) > eps:
+    if f(xMID)*f(xHI) < 0:  # solution in upper half
+        xLO = xMID
     else:
-        x_HI = x_MID
-    x_MID = (x_LO + x_HI)/2
-    itCnt += 1
+        xHI = xMID
+    xMID = (xLO + xHI) / 2
+    iters += 1
 
-print('Solution: {}'.format(x_MID))
-print('Number of iterations: {}'.format(itCnt))
-print('Check: f({:.8f}) = {:.8f}'.format(x_MID,f(x_MID)))
+print('Solution: {:.5f}'.format(xMID))
+print('Number of iterations: {}'.format(iters))
+print('Check: f({:.5f}) = {:.5f}'.format(xMID, f(xMID)))
