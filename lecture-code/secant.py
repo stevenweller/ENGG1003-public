9429fd3b81f14725146de60d2ee9f4121b1de2a3
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-04-28 22:07:42 +1000

    first

--- lecture-code/secant.py
+++ lecture-code/secant.py
@@ -1,21 +1,20 @@
 import numpy as np
 
 def f(x):
-#    return 0.0268*L**3 + 1.884*L**2 + 44.15*L - 500
-    return np.exp(-x)*np.sin(x) - np.cos(x)
+    return np.exp(x) - 3*x
 
 eps = 1e-6
-x0 = 6
-x1 = 10
-itCnt = 0       # iteration counter
+x0 = 0
+x1 = 1
+iters = 0       # iteration counter
 while abs(f(x1)) > eps:
     # line (=secant) through (x0,f(x)) and (x1,f(x1)) intersects
     # horizontal axis at (x,0)
     x = x1 - f(x1)*((x1 - x0)/(f(x1) - f(x0)))
     x0 = x1
     x1 = x
-    itCnt += 1
+    iters += 1
 
-print('Solution: {}'.format(x))
-print('Number of iterations: {}'.format(itCnt))
-print('Check: f({:.8f}) = {:.6f}'.format(x,f(x)))
+print('Solution: {:.5f}'.format(x))
+print('Number of iterations: {}'.format(iters))
+print('Check: f({:.5f}) = {:.5f}'.format(x, f(x)))
