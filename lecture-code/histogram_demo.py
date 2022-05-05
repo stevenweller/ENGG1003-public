3f78c4fcea24ebe5b8026f41517ce5f100e2df27
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-05-05 21:05:52 +1000

    first

--- lecture-code/histogram_demo.py
+++ lecture-code/histogram_demo.py
@@ -1,17 +1,12 @@
-# histogram_demo
 import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(1)
 d = np.random.normal(0.0, 1.0, size=100000)
 
-x = np.linspace(-5,5,num=1000)
+x = np.linspace(-5, 5, num=1001)
 f = 1/(np.sqrt(2 * np.pi)) * np.exp(-x**2 / 2)
 
-#plt.hist(d, 100)
-plt.hist(d, 100, density=True)
+plt.hist(d, bins=100, density=True) #area under histogram equal to 1
 plt.plot(x, f, color='r', linewidth=3)
-
-
-#plt.plot(d, 'o')
 plt.show()
