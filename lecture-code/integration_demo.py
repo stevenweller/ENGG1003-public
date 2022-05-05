8d2051288cae97ec4688de297fa5eccb504a0dd2
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-05-05 21:00:56 +1000

    first

---
+++ lecture-code/integration_demo.py
@@ -0,0 +1,13 @@
+import numpy as np
+import integration as integ
+
+def v(t):
+    return 3*t**2*np.exp(t**3)
+
+a = 0
+b = 1
+trap = integ.trapezoidal(v, a, b)
+print("Trapezoidal method: {:.5f}".format(trap))
+
+simp = integ.simpson(v, a, b)
+print("Trapezoidal method: {:.5f}".format(simp))
