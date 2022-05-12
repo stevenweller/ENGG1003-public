02a14227dfd253d9aafa2360eb029e20ca1e14f1
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-05-12 21:55:59 +1000

    second

--- lecture-code/fit_cubic.py
+++ lecture-code/fit_cubic.py
@@ -1,22 +1,28 @@
-# fit_cubic
 import numpy as np
-from scipy.optimize import curve_fit
+import scipy.optimize as sco
 import matplotlib.pyplot as plt
 
+
+# fit a cubic poly to data
 # guess data is a cubic
 def cubic(x, a, b, c, d):
     return a*x**3 + b*x**2 + c*x + d
 
-np.random.seed(1)   # replicate results by fixing seed
+
+np.random.seed(1)
 # data is actually a cubic + noise
 x = np.linspace(-4, 4, 100)
 y = -4*x**3 + 3*x**2 + 25*x + 6 + np.random.normal(0., 10, len(x))
-plt.plot(x, y, '.')
+plt.plot(x, y, 'r.')
 
-popt, pcov = curve_fit(cubic, x, y)
-a = popt[0]; b = popt[1]; c = popt[2]; d = popt[3]
+popt, _ = sco.curve_fit(cubic, x, y)
+a = popt[0]
+b = popt[1]
+c = popt[2]
+d = popt[3]
 
-plt.plot(x, cubic(x, a, b, c, d), 'r')
+plt.plot(x, cubic(x, a, b, c, d), 'b')
 plt.title('Best fit cubic {:.2f}x**3 + {:.2f}x**2 + {:.2f}x + {:.2f}'.format(a,b,c,d))
-plt.xlabel('x'); plt.ylabel('y')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
