ddbf0ffa329f17975698466625b54789ce7993b5
stevenweller <78775247+stevenweller@users.noreply.github.com>
2022-05-12 21:42:40 +1000

    first

---
+++ lecture-code/sea_ice.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+# dataset: September sea-ice extent 1979-2021
+# https://climate.nasa.gov/vital-signs/arctic-sea-ice/
+mydata = pd.read_csv("2485_Sept_Arctic_extent_1979-2021.csv")
+
+year = mydata['year'].values
+extent = mydata['extent'].values
+
+
+def line(t, m, b):
+    return m * t + b
+
+
+# STRAIGHT-LINE FIT
+popt, _ = curve_fit(line, year, extent)
+m = popt[0]
+b = popt[1]
+
+yearto2080 = range(1979, 2080)
+
+print('extent(yr) = {:.3f}*yr + {:.3f}'.format(m, b))
+print('Estimate September sea-ice extent is 0 in year = {}'.format(int(-b/m)))
+plt.plot(year, extent, 'o')
+plt.plot(yearto2080, line(yearto2080, m, b), 'r')
+plt.plot(-b/m, 0, 'gs')   # green square when ice extent is zero
+plt.xlabel('Year')
+plt.ylabel('September sea-ice extent (million sq km)')
+plt.grid()
+plt.show()
